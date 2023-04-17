3f3f28afa4e6d06600d4e554700b0e9e1c1c43c1
SeanCena <seansng1@gmail.com>
2023-04-17 04:08:55 -0700

    extended the api

--- index.js
+++ index.js
@@ -43,7 +43,12 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+    if (name != undefined && job != undefined) {
+        let result = findUserByNameAndJob(name, job);
+        result = {users_list: result};
+        res.send(result);
+    } else if (name != undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
@@ -98,6 +103,10 @@ function findUserById(id) {
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByNameAndJob = (name, job) => { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job ); 
 }
 
 app.listen(port, () => {
