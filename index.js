b76dd78fd93801d719d405801efbce4f35021314
SeanCena <seansng1@gmail.com>
2023-04-17 03:22:24 -0700

    basic express project

---
+++ index.js
@@ -0,0 +1,15 @@
+// Backend entrypoint
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
